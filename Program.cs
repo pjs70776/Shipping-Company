657098008999709b8e2bf78f702a5cbb36bd9887
pjs70776 <patrick.j.stover.1985@gmail.com>
2024-08-05 13:50:01 -0700

    update

--- Program.cs
+++ Program.cs
@@ -22,34 +22,39 @@ namespace Shipping_Company
             if (weight > 50)
             {
                 Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
             }
-            Console.ReadLine();
+            else
+            {
+                //Remaining user input
+                Console.WriteLine("Enter the package width");
+                int width = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the package height");
+                int height = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the package length");
+                int length = Convert.ToInt32(Console.ReadLine());
 
-            //Remaining user input
-            Console.WriteLine("Enter the package width");
-            int width = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the package height");
-            int height = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the package length");
-            int length = Convert.ToInt32(Console.ReadLine());
+                //Dimentions determination
+                if (width + height + length > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                }
+                else
+                {
+                    Console.WriteLine("Your package meets our specifications to be shipped.");
+                    //Package calculation
+                    int shippingCostNoWeight = (width * height * length);
+                    int ShippingCostPlusWeight = (shippingCostNoWeight * weight);
+                    int totalShippingCost = (ShippingCostPlusWeight / 100);
+                    Console.WriteLine("Your estimated total for shipping this package is: " + "$" + totalShippingCost + " Thank you!");
+                    Console.ReadLine();
+                }
+                Console.ReadLine();
+
+                
+            }
             
-            //Dimentions determination
-            if (width * height * length > 50)
-            {
-                Console.WriteLine("Package too big to be shipped via Package Express.");
-            }
-            else if (height * width * length < 50)
-            {
-                Console.WriteLine("Your package meets our specifications to be shipped.");
-            }
-            Console.ReadLine() ;
-
-            //Package calculation
-            int shippingCostNoWeight = (width * height * length);
-            int ShippingCostPlusWeight = (shippingCostNoWeight * weight);
-            int totalShippingCost = (ShippingCostPlusWeight / 100);
-            Console.WriteLine("Your estimated total for shipping this package is: " + "$" + totalShippingCost + " Thank you!");
-            Console.ReadLine();
+           
 
         }
     }
